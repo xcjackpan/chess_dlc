c7d32e25ce1caa55c69650ba3ba9fd8c14ecc174
xcjackpan <xcjackpan99@gmail.com>
2020-12-26 04:09:13 -0500

    firebase test

--- server/server.go
+++ server/server.go
@@ -5,9 +5,13 @@ import (
   "log"
   "net/http"
   "github.com/gorilla/mux"
+  "context"
+  firebase "firebase.google.com/go"
+  "google.golang.org/api/option"
 )
 
 const port = "8080"
+const databaseUrl = "https://chess-dlc-default-rtdb.firebaseio.com/"
 
 func enableCors(w *http.ResponseWriter) {
   (*w).Header().Set("Access-Control-Allow-Origin", "*")
@@ -21,6 +25,11 @@ func handleRoot(w http.ResponseWriter, r *http.Request) {
 func handleCreate(w http.ResponseWriter, r *http.Request) {
   enableCors(&w)
   fmt.Fprintf(w, "create")
+
+  // Initially called to create the game in the backend
+  // 1. Generates an unused ID
+  // 2. Sends it back to the client
+  // 3. Creates a record of the game in Redis
 }
 
 func handleRequests() {
@@ -31,6 +40,34 @@ func handleRequests() {
   router.HandleFunc("/create", handleCreate).Methods(http.MethodGet, http.MethodPut, http.MethodPatch, http.MethodOptions)
 
   log.Fatal(http.ListenAndServe(":"+port, router))
+}
+
+func sampleRead() {
+  ctx := context.Background()
+  conf := &firebase.Config{
+    DatabaseURL: databaseUrl,
+  }
+  // Fetch the service account key JSON file contents
+  opt := option.WithCredentialsFile("firebase_auth.json")
+
+  // Initialize the app with a service account, granting admin privileges
+  app, err := firebase.NewApp(ctx, conf, opt)
+  if err != nil {
+    log.Fatalln("Error initializing app:", err)
+  }
+
+  client, err := app.Database(ctx)
+  if err != nil {
+    log.Fatalln("Error initializing database client:", err)
+  }
+
+  // As an admin, the app has access to read and write all data, regardless of Security Rules
+  ref := client.NewRef("")
+  var data map[string]interface{}
+  if err := ref.Get(ctx, &data); err != nil {
+    log.Fatalln("Error reading from database:", err)
+  }
+  fmt.Println(data)
 }
 
 func main() {
